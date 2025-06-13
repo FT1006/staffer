e0173f1524424bc008314e06aca682114b640af6
FT1006 <frankthp1234@gmail.com>
2025-06-13 15:37:58 +0100

    implemented call_function to handle LLM function calls. Updated generate_content to use call_function

--- main.py
+++ main.py
@@ -3,24 +3,14 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 import sys
-from functions.get_files_info import schema_get_files_info
-from functions.get_file_content import schema_get_file_content
-from functions.write_file import schema_write_file
-from functions.run_python_file import schema_run_python_file
+from available_functions import get_available_functions, call_function
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
 client = genai.Client(api_key=api_key)
 
-available_functions = types.Tool(
-    function_declarations=[
-        schema_get_files_info,
-        schema_get_file_content,
-        schema_write_file,
-        schema_run_python_file,
-    ]
-)
+available_functions = get_available_functions()
 
 if len(sys.argv) == 1:
     print("Error: No prompt provided")
@@ -59,13 +49,24 @@ res = client.models.generate_content(
 promptTokens = res.usage_metadata.prompt_token_count
 responseTokens = res.usage_metadata.candidates_token_count
 
-if len(sys.argv) > 2 and "--verbose" in sys.argv:
+verbose = len(sys.argv) > 2 and "--verbose" in sys.argv
+
+if verbose:
     print(f"User prompt: {prompt}")
-    print(res.text)
-    print(f"Prompt tokens: {promptTokens}")
-    print(f"Response tokens: {responseTokens}")
-elif res.function_calls:
+
+if res.function_calls:
     for function_call_part in res.function_calls:
-        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+        if verbose:
+            function_call_result = call_function(function_call_part, verbose=True)
+        else:
+            function_call_result = call_function(function_call_part)
+        if not function_call_result.parts[0].function_response.response:
+            sys.exit(1)
+        else:
+            print(f"-> {function_call_result.parts[0].function_response.response}")
 else:
     print(res.text)
+
+if verbose:
+    print(f"Prompt tokens: {promptTokens}")
+    print(f"Response tokens: {responseTokens}")
